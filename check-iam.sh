b8d5a15adba76268d69969b6aa7506589b805339
Sid <paliwals7@outlook.com>
2026-06-16 12:12:02 +0530

    Updated back check-iam.sh

--- check-iam.sh
+++ check-iam.sh
@@ -1,38 +1,7 @@
 #!/bin/bash
 
-set -uo pipefail
-
-###############################################################################
-
-# PRECHECKS
-
-###############################################################################
-
-if [[ ! -f "./common.sh" ]]
-then
-echo
-echo "ERROR: common.sh not found"
-echo
-exit 1
-fi
-
-if [[ ! -f "./config.conf" ]]
-then
-echo
-echo "ERROR: config.conf not found"
-echo "Run ./bootstrap.sh first"
-echo
-exit 1
-fi
-
 source ./common.sh
 source ./config.conf
-
-###############################################################################
-
-# VARIABLES
-
-###############################################################################
 
 SERVICE="IAM"
 
@@ -43,25 +12,17 @@ HTML_FILE="reports/iam-report.html"
 
 init_csv "$REPORT_FILE"
 
-echo
-echo "Starting IAM Audit..."
-echo
-
 ###############################################################################
-
 # ROOT MFA
-
 ###############################################################################
 
-ROOT_MFA=$(aws iam get-account-summary 
---query 'SummaryMap.AccountMFAEnabled' 
+ROOT_MFA=$(aws iam get-account-summary \
+--query 'SummaryMap.AccountMFAEnabled' \
 --output text 2>/dev/null)
 
 if [[ "$ROOT_MFA" == "1" ]]
 then
-
-```
-write_result \
+    write_result \
     global \
     IAM \
     ROOT_MFA \
@@ -69,12 +30,8 @@ write_result \
     PASS \
     CRITICAL \
     "Root MFA enabled"
-```
-
 else
-
-```
-write_result \
+    write_result \
     global \
     IAM \
     ROOT_MFA \
@@ -82,25 +39,19 @@ write_result \
     FAIL \
     CRITICAL \
     "Root MFA NOT enabled"
-```
-
 fi
 
 ###############################################################################
-
 # ROOT ACCESS KEYS
-
 ###############################################################################
 
-ROOT_KEYS=$(aws iam get-account-summary 
---query 'SummaryMap.AccountAccessKeysPresent' 
+ROOT_KEYS=$(aws iam get-account-summary \
+--query 'SummaryMap.AccountAccessKeysPresent' \
 --output text 2>/dev/null)
 
 if [[ "$ROOT_KEYS" == "0" ]]
 then
-
-```
-write_result \
+    write_result \
     global \
     IAM \
     ROOT_ACCESS_KEYS \
@@ -108,12 +59,8 @@ write_result \
     PASS \
     HIGH \
     "No root access keys"
-```
-
 else
-
-```
-write_result \
+    write_result \
     global \
     IAM \
     ROOT_ACCESS_KEYS \
@@ -121,21 +68,17 @@ write_result \
     FAIL \
     HIGH \
     "Root access keys present"
-```
-
 fi
 
 ###############################################################################
-
 # PASSWORD POLICY
-
 ###############################################################################
 
-if aws iam get-account-password-policy >/dev/null 2>&1
-then
+aws iam get-account-password-policy >/dev/null 2>&1
 
-```
-write_result \
+if [[ $? -eq 0 ]]
+then
+    write_result \
     global \
     IAM \
     PASSWORD_POLICY \
@@ -143,12 +86,8 @@ write_result \
     PASS \
     MEDIUM \
     "Password policy configured"
-```
-
 else
-
-```
-write_result \
+    write_result \
     global \
     IAM \
     PASSWORD_POLICY \
@@ -156,55 +95,27 @@ write_result \
     FAIL \
     MEDIUM \
     "Password policy missing"
-```
-
 fi
 
 ###############################################################################
-
-# IAM USERS
-
+# USERS
 ###############################################################################
 
-USERS=$(aws iam list-users 
---query 'Users[*].UserName' 
+USERS=$(aws iam list-users \
+--query 'Users[*].UserName' \
 --output text 2>/dev/null)
-
-if [[ -z "$USERS" ]]
-then
-
-```
-write_result \
-    global \
-    IAM \
-    USER_DISCOVERY \
-    N/A \
-    INFO \
-    INFO \
-    "No IAM users found"
-```
-
-fi
-
-###############################################################################
-
-# USER MFA
-
-###############################################################################
 
 for USER in $USERS
 do
 
-```
-MFA=$(aws iam list-mfa-devices \
+    MFA=$(aws iam list-mfa-devices \
     --user-name "$USER" \
     --query 'MFADevices[*].SerialNumber' \
     --output text 2>/dev/null)
 
-if [[ -z "$MFA" ]]
-then
-
-    write_result \
+    if [[ -z "$MFA" ]]
+    then
+        write_result \
         global \
         IAM \
         USER_MFA \
@@ -212,10 +123,8 @@ then
         FAIL \
         HIGH \
         "User without MFA"
-
-else
-
-    write_result \
+    else
+        write_result \
         global \
         IAM \
         USER_MFA \
@@ -223,38 +132,30 @@ else
         PASS \
         LOW \
         "MFA enabled"
-
-fi
-```
+    fi
 
 done
 
 ###############################################################################
-
-# ACCESS KEYS AGE
-
+# ACCESS KEYS OLDER THAN 90 DAYS
 ###############################################################################
 
 for USER in $USERS
 do
 
-```
-aws iam list-access-keys \
+    aws iam list-access-keys \
     --user-name "$USER" \
     --query 'AccessKeyMetadata[*].[AccessKeyId,CreateDate]' \
     --output text 2>/dev/null |
 
-while read KEY CREATED
-do
+    while read KEY CREATED
+    do
 
-    [[ -z "$KEY" ]] && continue
+        AGE=$(( ($(date +%s) - $(date -d "$CREATED" +%s)) / 86400 ))
 
-    AGE=$(( ($(date +%s) - $(date -d "$CREATED" +%s)) / 86400 ))
-
-    if (( AGE > 90 ))
-    then
-
-        write_result \
+        if (( AGE > 90 ))
+        then
+            write_result \
             global \
             IAM \
             OLD_ACCESS_KEY \
@@ -262,10 +163,8 @@ do
             FAIL \
             HIGH \
             "Access key age=$AGE days"
-
-    else
-
-        write_result \
+        else
+            write_result \
             global \
             IAM \
             OLD_ACCESS_KEY \
@@ -273,33 +172,27 @@ do
             PASS \
             LOW \
             "Access key age=$AGE days"
+        fi
 
-    fi
-
-done
-```
+    done
 
 done
 
 ###############################################################################
-
-# ADMINISTRATOR ACCESS POLICY
-
+# ADMINISTRATOR ACCESS
 ###############################################################################
 
 for USER in $USERS
 do
 
-```
-POLICIES=$(aws iam list-attached-user-policies \
+    POLICIES=$(aws iam list-attached-user-policies \
     --user-name "$USER" \
     --query 'AttachedPolicies[*].PolicyName' \
     --output text 2>/dev/null)
 
-if echo "$POLICIES" | grep -q "AdministratorAccess"
-then
-
-    write_result \
+    if echo "$POLICIES" | grep -q AdministratorAccess
+    then
+        write_result \
         global \
         IAM \
         ADMIN_ACCESS \
@@ -307,44 +200,29 @@ then
         FAIL \
         HIGH \
         "AdministratorAccess attached"
-
-fi
-```
+    fi
 
 done
 
 ###############################################################################
-
 # HTML REPORT
-
 ###############################################################################
 
-generate_html 
-"$REPORT_FILE" 
+generate_html "$REPORT_FILE" "$HTML_FILE"
+
+###############################################################################
+# S3 UPLOAD
+###############################################################################
+
+create_bucket_if_needed
+
+upload_reports \
+IAM \
+"$REPORT_FILE" \
 "$HTML_FILE"
 
 ###############################################################################
-
-# S3 UPLOAD
-
-###############################################################################
-
-if validate_bucket
-then
-
-```
-upload_reports \
-    IAM \
-    "$REPORT_FILE" \
-    "$HTML_FILE"
-```
-
-fi
-
-###############################################################################
-
 # COMPLETE
-
 ###############################################################################
 
 echo
